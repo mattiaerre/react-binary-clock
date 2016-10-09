caa4c88831f2b560c894c80e32c0662f045bfff2
mattia richetto <mrichetto@opentable.com>
2016-10-09 20:24:44 +0100

    remove description from wikipedia

--- test/View.spec.js
+++ test/View.spec.js
@@ -47,11 +47,3 @@ describe('<View />', () => {
     });
   });
 });
-
-/*
-
-As of 2008, the most common binary clocks sold are designed by Anelace Inc., and use six columns of LEDs to represent zeros and ones. Each column represents a single decimal digit, a format known as binary-coded decimal (BCD). The bottom row in each column represents 1 (or 20), with each row above representing higher powers of two, up to 23 (or 8).
-
-To read each individual digit in the time, the user adds the values that each illuminated LED represents, then reads these from left to right. The first two columns represent the hour, the next two represent the minute and the last two represent the second. Since zero digits are not illuminated, the positions of each digit must be memorized if the clock is to be usable in the dark.
-
-*/
